b38e129ceda38e37acde492b271ea7930c7df98a
dishaagrawalcodes <dishaagrawal9568@gmail.com>
2025-01-06 15:50:38 +0530

    completed BestSeller code

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Hero from '../Components/Hero'
 import LatestCollection from '../Components/LatestCollection'
+import BestSeller from '../Components/BestSeller'
+import Ourpolicy from '../Components/Ourpolicy'
 const Home = () => {
   return (
     <div>
       <Hero/>
       <LatestCollection/>
+      <BestSeller/>
+      <Ourpolicy/>
     </div>
   )
 }
